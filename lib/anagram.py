74899c695d9baaf049c3c80886b6854d365a5636
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-05-13 08:51:28 +0300

    creates anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+       
+        self.word = word.lower()
+
+    def match(self, word_list):
+        
+        sorted_word = sorted(self.word)
+
+        return [word for word in word_list if sorted(word.lower()) == sorted_word]
+
+
+
+listen = Anagram("listen")
+listen.match(["listen", "silent", "hippopotamus"])
